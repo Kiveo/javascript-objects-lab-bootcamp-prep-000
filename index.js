6ade7a34e5e0d739479738a5d2fc6b4cd734edd2
Maverick Tesini <mavericktesini@gmail.com>
2018-02-05 18:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
   // var newkeyvalue = {key: value};
-  var newObj = Object.assign({object});
+  var newObj = Object.assign({value});
   return newObj;
 }
