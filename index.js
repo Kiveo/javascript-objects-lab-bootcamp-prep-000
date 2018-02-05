889f765fd1e95fd793402438ee728f4ca7ebd8aa
Maverick Tesini <mavericktesini@gmail.com>
2018-02-05 18:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue (object, key, value) {
   var newkeyvalue = {key: value};
-  var newObj = Object.assign({}, newkeyvalue);
+  var newObj = Object.assign({}, newkeyvalue); //object parameter intelligently detected. Neat.
   return newObj;
 }
